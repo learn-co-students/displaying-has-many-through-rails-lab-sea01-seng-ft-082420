35ea957482e084de54face4b6b6a07d43bea4a80
Chanson9892 <chan.hanson5@gmail.com>
2020-09-19 17:20:39 -0700

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,16 @@
+class DoctorsController < ApplicationController
+    before_action :set_doctor, only: [:show]
+
+    def show
+    end
+
+    private
+    def set_doctor
+        @doctor = Doctor.find(params[:id])
+    end
+
+    def doctor_params
+        params.require(:doctor).permit(:name, :department)
+    end
+
+end
